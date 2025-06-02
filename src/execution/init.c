b12a5eda36cd5895582f7ad55a15316762975367
4n4k1n <anakin@pregitzer.de>
2025-06-02 12:29:44 +0200

    ADD(init.c) created own file for init functions

---
+++ src/execution/init.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 11:41:24 by apregitz          #+#    #+#             */
+/*   Updated: 2025/06/02 11:41:42 by apregitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	init_exec_data(t_mini *mini)
+{
+	mini->exec_data.ep = env_list_to_array(&mini->env_list);
+	mini->exec_data.input_file = STDIN_FILENO;
+	mini->exec_data.output_file = STDOUT_FILENO;
+}

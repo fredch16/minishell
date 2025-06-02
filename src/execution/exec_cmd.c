711a126748918157a51f5df6e7df28823f6857d8
4n4k1n <anakin@pregitzer.de>
2025-06-02 12:08:19 +0200

    ADD(exec_cmd.c) function to set up the pipe adn for the proccess adnd one for execution the command

---
+++ src/execution/exec_cmd.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exec_cmd.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/30 16:31:44 by apregitz          #+#    #+#             */
+/*   Updated: 2025/06/02 12:03:32 by apregitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	exec_cmd(t_cmd_node *cmd_node, t_mini *mini)
+{
+	setup_child_input(&mini->exec_data);
+	setup_child_output(&mini->exec_data);
+	if (handle_redirections(cmd_node ,mini) == -1)
+		ft_error(1, NULL);
+	execve(cmd_node->path, cmd_node->cmd, mini->exec_data.ep);
+	ft_error(127, "execve");
+}
+
+void	setup_exec(t_cmd_node *cmd_node, t_mini *mini)
+{
+	pid_t	pid;
+
+	if (pipe(cmd_node->fd) == -1)
+		ft_error(1, "pipe");
+	pid = fork();
+	if (pid == -1)	
+		ft_error(1, "fork");
+	if (pid == 0)
+		exec_cmd(cmd_node, mini);
+}

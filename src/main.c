83ed508669e97cbd8198dab5bbc5436752456689
4n4k1n <anakin@pregitzer.de>
2025-06-03 06:45:24 +0200

    ADD(main.c utilities/) new fiels from the merge

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 21:58:05 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/02 16:07:22 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/06/02 17:57:35 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,7 @@ int	main(int ac, char **av, char **env)
 			continue;
 		}
 		print_cmd_list(mini.cmd_list);
+		execution(&mini);
 		free(line);
 		gc_free_by_type(GC_PARSE);
 	}

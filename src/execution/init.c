5a028a7aa4622ee63045041aca98492ba84a996e
4n4k1n <anakin@pregitzer.de>
2025-06-03 12:01:59 +0200

    FIX(init.c) fixed dereferencing

--- src/execution/init.c
+++ src/execution/init.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 11:41:24 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/02 11:41:42 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/03 07:11:30 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	init_exec_data(t_mini *mini)
 {
-	mini->exec_data.ep = env_list_to_array(&mini->env_list);
+	mini->exec_data.ep = env_list_to_array(mini->env_list);
 	mini->exec_data.input_file = STDIN_FILENO;
 	mini->exec_data.output_file = STDOUT_FILENO;
 }

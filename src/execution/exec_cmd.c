1927ac241ae5469f664975bd09a109186e6546ea
4n4k1n <anakin@pregitzer.de>
2025-06-04 07:31:16 +0200

    FEAT(exec_cmd.c) fixed the cmd_exec function

--- src/execution/exec_cmd.c
+++ src/execution/exec_cmd.c
@@ -6,19 +6,24 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 16:31:44 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/02 12:38:13 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/04 06:06:49 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+#include "../../include/minishell.h"
 
 void	exec_cmd(t_cmd_node *cmd_node, t_mini *mini)
 {
-	setup_child_input(&mini->exec_data);
-	setup_child_output(&mini->exec_data);
-	if (handle_redirections(cmd_node ,mini) == -1)
+	char	*cmd_path;
+
+	setup_child_input(mini);
+	setup_child_output(mini);
+	if (handle_redirections(cmd_node, mini) == -1)
 		ft_error(1, NULL);
-	execve(cmd_node->path, cmd_node->cmd, mini->exec_data.ep);
+	cmd_path = get_command_path(cmd_node, mini);
+	if (!cmd_path)
+		ft_error(127, "Command not found");
+	execve(cmd_path, cmd_node->cmd, mini->exec_data.ep);
 	ft_error(127, "execve");
 }
 
@@ -26,7 +31,7 @@ void	setup_exec(t_cmd_node *cmd_node, t_mini *mini)
 {
 	pid_t	pid;
 
-	if (cmd_node->next || pipe(cmd_node->fd) == -1)
+	if (cmd_node->next && pipe(cmd_node->fd) == -1)
 		ft_error(1, "pipe");
 	pid = fork();
 	if (pid == -1)	

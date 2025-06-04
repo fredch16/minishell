be2433e88e8bef9e8431af4585850132b979397c
4n4k1n <anakin@pregitzer.de>
2025-06-04 12:12:03 +0200

    FEAT(error.c) removed clear_fd_list function from error function

--- src/execution/error.c
+++ src/execution/error.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:25:21 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/04 06:09:47 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/04 12:06:40 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,6 @@ void	ft_error(int error_code, char *error_msg)
 {
     perror(error_msg);
     gc_free_all();
-    clear_fd_list();
+    // clear_fd_list();
     exit(error_code);
 }

eabd04ce5e6e8afbaa44d7d6bc7770a06dd62435
Fred Charbonnier <frederickcharbonnier@icloud.com>
2025-06-04 15:31:26 +0200

    FIX () - fixed an issue causing strlcat to be run on uninitialised value when removing non existent quotes

--- include/libft/ft_charjoin.c
+++ include/libft/ft_charjoin.c
@@ -6,7 +6,7 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 14:33:55 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/04 15:21:33 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/06/04 15:29:04 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 char	*ft_charjoin(char const *s1, char const s2)
 {
-	int		totallen;
-	char	*new;
-
-	if (!s2)
-		return (ft_strdup((char *)s1));
-	totallen = ft_strlen((char *)s1) + 1;
-	if (totallen == 0)
-		return (ft_strdup(""));
-	new = (char *)malloc(sizeof (char) * totallen + 1);
-	if (!new)
+	size_t	s1_len;
+	char	*result;
+	
+	if (!s1)
 		return (NULL);
-	ft_strlcpy(new, (char *)s1, ft_strlen((char *)s1) + 1);
-	ft_strlcat(new, &s2, totallen + 1);
-	return (new);
+	
+	s1_len = ft_strlen(s1);
+	result = (char *)malloc(sizeof(char) * (s1_len + 2));
+	if (!result)
+		return (NULL);
+	
+	ft_strlcpy(result, s1, s1_len + 1);
+	result[s1_len] = s2;
+	result[s1_len + 1] = '\0';
+	
+	return (result);
 }

d619e02aefae0ac010f99e65bf0244b32b1767e2
4n4k1n <anakin@pregitzer.de>
2025-06-04 05:19:10 +0200

    BUILD(libft/) moved libft from src/ directory to include/ directory

--- include/libft/ft_charjoin.c
+++ include/libft/ft_charjoin.c
@@ -6,31 +6,24 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/30 14:33:55 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/03 22:59:12 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/05/30 14:39:10 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-/*
- * Joins a string and a character, returning a new string
- * @param s1 The string to append to
- * @param s2 The character to append
- * @return A new string with s2 appended to s1, or NULL if allocation fails
- */
 char	*ft_charjoin(char const *s1, char const s2)
 {
-	size_t	len;
-	char	*result;
+	int		totallen;
+	char	*new;
 
-	if (!s1)
-		s1 = "";
-	len = ft_strlen(s1);
-	result = (char *)malloc(sizeof(char) * (len + 2));
-	if (!result)
+	totallen = ft_strlen((char *)s1) + 1;
+	if (totallen == 0)
+		return (ft_strdup(""));
+	new = (char *)malloc(sizeof (char) * totallen + 1);
+	if (!new)
 		return (NULL);
-	ft_strlcpy(result, s1, len + 1);
-	result[len] = s2;
-	result[len + 1] = '\0';
-	return (result);
+	ft_strlcpy(new, (char *)s1, ft_strlen((char *)s1) + 1);
+	ft_strlcat(new, &s2, totallen + 1);
+	return (new);
 }
---
+++ include/libft/libft.h
@@ -0,0 +1,117 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/10 12:04:28 by fredchar          #+#    #+#             */
+<<<<<<< HEAD:include/libft/libft.h
+/*   Updated: 2025/06/04 00:29:59 by fredchar         ###   ########.fr       */
+=======
+/*   Updated: 2025/05/30 14:38:23 by fredchar         ###   ########.fr       */
+>>>>>>> execution:src/libft/libft.h
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h> // For ft_printf
+# include "ft_printf.h"
+# include "get_next_line.h"
+# include <stdint.h>
+
+// struct for the linked list functions
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+// basic functions
+
+int				ft_isalnum(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isprint(int c);
+int				ft_isascii(int c);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+size_t			ft_strlen(const char *str);
+int				ft_atoi(char *str);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memset(void *s, int c, size_t n);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
+char			*ft_strnstr(const char *big, const char *little, size_t len);
+int				ft_strncmp(const char *s1, const char *s2, unsigned int n);
+<<<<<<< HEAD:include/libft/libft.h
+int				ft_strcmp(const char *s1, const char *s2);
+=======
+>>>>>>> execution:src/libft/libft.h
+int				ft_atoi(char *str);
+unsigned int	ft_strlcat(char *dest, const char *src, unsigned int size);
+unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int dstsize);
+size_t			ft_strs_len(char **str);
+
+// memory functions
+
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+void			*ft_memcpy(void *dest, const void *src, size_t n);
+void			*ft_memmove(void *dest, const void *src, size_t n);
+void			ft_free_array(char **array);
+
+// malloc / harder functions
+
+void			*ft_calloc(size_t count, size_t size);
+char			*ft_strdup(char *str);
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_charjoin(char const *s1, char const s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			*ft_itoa(int n);
+char			**ft_split(char const *s, char c);
+
+// free stuff
+
+void			ft_free_char2(char **tofree);
+void			ft_free_char3(char ***tofree);
+
+// functions in functions
+
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_striteri(char *s, void (*f)(unsigned int, char *));
+
+// FD functions
+
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int nb, int fd);
+
+// ft_printf function
+
+int				ft_printf(const char *format, ...);
+
+// get_next_line function
+
+char			*get_next_line(int fd);
+
+//	bonus / linked list functions
+
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_front(t_list **lst, t_list *new);
+int				ft_lstsize(t_list *lst);
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void *));
+void			ft_lstiter(t_list *list, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst, void *(*f)(void*), void (*del)(void *));
+
+#endif

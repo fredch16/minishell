24fa4232d87b2c5001874e48947be4508a3b686c
4n4k1n <anakin@pregitzer.de>
2025-06-04 07:25:04 +0200

    ADD(pipes_utils.c) util functions for the pipes

---
+++ src/execution/pipes_utils.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipes_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/04 06:04:46 by apregitz          #+#    #+#             */
+/*   Updated: 2025/06/04 06:04:48 by apregitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+int	**create_pipes(int pipe_count)
+{
+	int	**pipes;
+	int	i;
+
+	pipes = malloc(sizeof(int *) * pipe_count);
+	if (!pipes)
+		return (NULL);
+	i = 0;
+	while (i < pipe_count)
+	{
+		pipes[i] = malloc(sizeof(int) * 2);
+		if (!pipes[i] || pipe(pipes[i]) == -1)
+		{
+			while (--i >= 0)
+				free(pipes[i]);
+			free(pipes);
+			return (NULL);
+		}
+		i++;
+	}
+	return (pipes);
+}
+
+void	close_all_pipes(int **pipes, int pipe_count)
+{
+	int	i;
+
+	if (!pipes)
+		return ;
+	i = 0;
+	while (i < pipe_count)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		free(pipes[i]);
+		i++;
+	}
+	free(pipes);
+}
+
+int	cleanup_pipeline(int **pipes, pid_t *pids, int pipe_count, int ret_val)
+{
+	if (pids)
+		free(pids);
+	if (pipes)
+		close_all_pipes(pipes, pipe_count);
+	return (ret_val);
+}
+
+int	finalize_pipeline(int **pipes, pid_t *pids, t_mini *mini, int pipe_count)
+{
+	int	exit_code;
+
+	if (pipes)
+		close_all_pipes(pipes, pipe_count);
+	exit_code = wait_for_children(pids, mini->cmd_list->size);
+	free(pids);
+	return (exit_code);
+}

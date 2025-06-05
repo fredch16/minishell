50f9aeadce2ab0dfcbe82c3c2ce88178d522d947
4n4k1n <anakin@pregitzer.de>
2025-06-05 07:03:20 +0200

    added pipes_count, fail check for the allocation adn close the pipes at the end

--- src/execution/exec_pipes.c
+++ src/execution/exec_pipes.c
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 06:24:21 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/04 15:33:14 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/05 06:37:57 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,19 @@ int	execute_pipeline(t_mini *mini)
 	int			**pipes;
 	pid_t		*pids;
 	int			i;
+	int			pipe_count;
 
+	pipe_count = mini->cmd_list->size - 1;
 	pipes = NULL;
-	if (mini->cmd_list->size > 1)
+	if (pipe_count > 0)
 	{
-		pipes = create_pipes(mini->cmd_list->size - 1);
+		pipes = create_pipes(pipe_count);
 		if (!pipes)
 			return (1);
 	}
 	pids = malloc(sizeof(pid_t) * mini->cmd_list->size);
 	if (!pids)
-		return (cleanup_pipeline(pipes, NULL, mini->cmd_list->size - 1, 1));
+		return (cleanup_pipeline(pipes, NULL, pipe_count, 1));
 	gc_track(pids, GC_EXEC);
 	cmd = mini->cmd_list->head;
 	i = 0;
@@ -36,12 +38,53 @@ int	execute_pipeline(t_mini *mini)
 	{
 		pids[i] = fork_and_exec(cmd, mini, pipes, i);
 		if (pids[i] == -1)
-			return (cleanup_pipeline(pipes, pids, mini->cmd_list->size - 1, 1));
+			return (cleanup_pipeline(pipes, pids, pipe_count, 1));
+		if (i > 0 && pipes)
+		{
+			close(pipes[i - 1][0]);
+			close(pipes[i - 1][1]);
+		}
 		cmd = cmd->next;
 		i++;
 	}
-	return (finalize_pipeline(pipes, pids, mini, mini->cmd_list->size - 1));
+	if (pipes && pipe_count > 0)
+	{
+		close(pipes[pipe_count - 1][0]);
+		close(pipes[pipe_count - 1][1]);
+	}
+	return (finalize_pipeline(pipes, pids, mini, pipe_count));
 }
+
+// int	execute_pipeline(t_mini *mini)
+// {
+// 	t_cmd_node	*cmd;
+// 	int			**pipes;
+// 	pid_t		*pids;
+// 	int			i;
+
+// 	pipes = NULL;
+// 	if (mini->cmd_list->size > 1)
+// 	{
+// 		pipes = create_pipes(mini->cmd_list->size - 1);
+// 		if (!pipes)
+// 			return (1);
+// 	}
+// 	pids = malloc(sizeof(pid_t) * mini->cmd_list->size);
+// 	if (!pids)
+// 		return (cleanup_pipeline(pipes, NULL, mini->cmd_list->size - 1, 1));
+// 	gc_track(pids, GC_EXEC);
+// 	cmd = mini->cmd_list->head;
+// 	i = 0;
+// 	while (cmd)
+// 	{
+// 		pids[i] = fork_and_exec(cmd, mini, pipes, i);
+// 		if (pids[i] == -1)
+// 			return (cleanup_pipeline(pipes, pids, mini->cmd_list->size - 1, 1));
+// 		cmd = cmd->next;
+// 		i++;
+// 	}
+// 	return (finalize_pipeline(pipes, pids, mini, mini->cmd_list->size - 1));
+// }
 
 // int	execute_pipeline(t_mini *mini)
 // {

eb4d3958dd09dc7e99b500a8ecf88956ff823e1b
4n4k1n <anakin@pregitzer.de>
2025-06-05 07:00:47 +0200

    FEAT(pipes.c) set up the pipes correctly

--- src/execution/pipes.c
+++ src/execution/pipes.c
@@ -6,54 +6,47 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 05:34:17 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/04 11:43:59 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/05 06:36:05 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-static void	close_unused_pipes(int **pipes, int cmd_index, int pipe_count)
+static void	setup_child_pipes(int **pipes, int cmd_index, int pipe_count)
 {
 	int	i;
 
-	i = 0;
-	while (i < pipe_count)
-	{
-		if (i != cmd_index - 1)
-			close(pipes[i][0]);
-		if (i != cmd_index)
-			close(pipes[i][1]);
-		i++;
-	}
-}
-
-static void	setup_child_pipes(int **pipes, int cmd_index, int pipe_count, t_mini *mini)
-{
 	if (cmd_index > 0)
 	{
-		dup2(pipes[cmd_index - 1][0], STDIN_FILENO);
-		close(pipes[cmd_index - 1][0]);
+		if (dup2(pipes[cmd_index - 1][0], STDIN_FILENO) == -1)
+			ft_error(1, "dup2 input failed");
 	}
 	if (cmd_index < pipe_count)
 	{
-		dup2(pipes[cmd_index][1], STDOUT_FILENO);
-		close(pipes[cmd_index][1]);
+		if (dup2(pipes[cmd_index][1], STDOUT_FILENO) == -1)
+			ft_error(1, "dup2 output failed");
 	}
-	close_unused_pipes(pipes, cmd_index, pipe_count);
-	setup_child_input(mini);
-	setup_child_output(mini);
+	i = 0;
+	while (i < pipe_count)
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+		i++;
+	}
 }
 
 int	fork_and_exec(t_cmd_node *cmd, t_mini *mini, int **pipes, int index)
 {
 	pid_t	pid;
+	int		pipe_count;
 
+	pipe_count = mini->cmd_list->size - 1;
 	pid = fork();
 	if (pid == -1)
 		return (-1);
 	if (pid == 0)
 	{
-		setup_child_pipes(pipes, index, mini->cmd_list->size - 1, mini);
+		setup_child_pipes(pipes, index, pipe_count);
 		if (handle_redirections(cmd, mini) == -1)
 			ft_error(1, "Redirection failed");
 		if (cmd->cmd_type == BUILTIN)

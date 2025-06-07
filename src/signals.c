ce1c64ed97e2589e115fc080b98fcd567f60fd79
Fred Charbonnier <frederickcharbonnier@icloud.com>
2025-06-07 19:56:38 +0200

    FIX(pipes) - fixed an issue causing memory leaks in case of interrupting processing before they had completed and were able to close respective pipes

--- src/signals.c
+++ src/signals.c
@@ -4,7 +4,7 @@
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
+/*                                                +#+#+#+#+#+   +#+          */
 /*   Created: 2025/06/07 16:35:11 by fredchar          #+#    #+#             */
 /*   Updated: 2025/06/07 19:10:43 by fredchar         ###   ########.fr       */
 /*                                                                            */
@@ -27,6 +27,25 @@ void	heredoc_sigint_hanlder(int signo)
 {
 	(void)signo;
 	g_signal_recieved = 1;
+}
+
+/**
+ * Set up signal handlers for when the shell is executing commands
+ * This prevents the shell from reacting to signals meant for child processes
+ */
+void	setup_parent_signals_for_execution(void)
+{
+	signal(SIGINT, SIG_IGN);  // Ignore SIGINT in the parent during execution
+	signal(SIGQUIT, SIG_IGN); // Continue ignoring SIGQUIT
+}
+
+/**
+ * Reset signal handlers for when the shell is ready for input
+ */
+void	reset_parent_signals(void)
+{
+	signal(SIGINT, handle_sigint);
+	signal(SIGQUIT, SIG_IGN);
 }
 
 void	setup_heredoc_signals(void)

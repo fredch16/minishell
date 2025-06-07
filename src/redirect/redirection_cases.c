50e1c3a53e70a6a8d0b84163d57f6a0a1959392e
4n4k1n <anakin@pregitzer.de>
2025-06-07 08:01:28 +0200

    FEAT(redirection_cases.c) forgot to close the fd after open and fixed the rong dup2 cmd

--- src/redirect/redirection_cases.c
+++ src/redirect/redirection_cases.c
@@ -6,17 +6,11 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 16:11:05 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/07 07:45:25 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/07 07:58:57 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
-
-// void	close_if_not_std(int fd, int std_fd)
-// {
-// 	if (fd != std_fd && fd != -1)
-// 		close(fd);
-// }
 
 int	handle_heredoc_redir(char *delimiter, t_mini *mini)
 {
@@ -28,6 +22,7 @@ int	handle_heredoc_redir(char *delimiter, t_mini *mini)
 		ft_error(1, "open", 0);
 	if (dup2(fd, STDIN_FILENO) == -1)
 		ft_error(1, "dup2", 0);
+	close(fd);
 	return (0);
 }
 
@@ -41,6 +36,7 @@ int	handle_input_redir(t_file_node *file_node, t_mini *mini)
 		ft_error(1, "open", 0);
 	if (dup2(fd, STDIN_FILENO) == -1)
 		ft_error(1, "dup2", 0);
+	close(fd);
 	return (0);
 }
 
@@ -52,8 +48,9 @@ int	handle_output_redir(t_file_node *file_node, t_mini *mini)
 	fd = open(file_node->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd == -1)
 		ft_error(1, "open", 0);
-	if (dup2(fd, STDIN_FILENO) == -1)
+	if (dup2(fd, STDOUT_FILENO) == -1)
 		ft_error(1, "dup2", 0);
+	close(fd);
 	return (0);
 }
 
@@ -65,7 +62,8 @@ int	handle_append_redir(t_file_node *file_node, t_mini *mini)
 	fd = open(file_node->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if (fd == -1)
 		ft_error(1, "open", 0);
-	if (dup2(fd, STDIN_FILENO) == -1)
+	if (dup2(fd, STDOUT_FILENO) == -1)
 		ft_error(1, "dup2", 0);
+	close(fd);
 	return (0);
 }

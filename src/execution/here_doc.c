055177750e474cbd0993dda3e23c52abf66c05fe
4n4k1n <anakin@pregitzer.de>
2025-06-11 18:06:03 +0200

    FEAT(here_doc.c) - added child process for the heredoc

--- src/execution/here_doc.c
+++ src/execution/here_doc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   here_doc.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 13:13:39 by apregitz          #+#    #+#             */
-/*   Updated: 2025/06/10 17:37:02 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/06/11 08:58:12 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,10 +41,7 @@ static void	read_heredoc_lines(t_mini *mini, char *delimiter, int write_fd, t_cm
 		if (!line)
 			break ;
 		if (is_delimiter(line, delimiter))
-		{
-			free(line);
-			break ;
-		}
+			return (free(line));
 		if (g_signal_recieved == 3)
 		{
 			if (line)
@@ -55,17 +52,32 @@ static void	read_heredoc_lines(t_mini *mini, char *delimiter, int write_fd, t_cm
 		if (!line)
 			break ;
 		if (cmd_node && cmd_node->cmd && cmd_node->cmd[0])
-			write_heredoc_line(write_fd, line);	
+			write_heredoc_line(write_fd, line);
+		free(line);
 	}
 }
 
 int	create_heredoc(char *delimiter, t_mini *mini, t_cmd_node *cmd_node, int builtin)
 {
-	int	pipefd[2];
+	int		pipefd[2];
+	pid_t	pid;
+	int		status;
 
 	if (pipe(pipefd) == -1)
 		return (perror("pipe"), -1);
-	read_heredoc_lines(mini, delimiter, pipefd[1], cmd_node, builtin);
+	pid = fork();
+	if (pid == -1)
+		return (close(pipefd[0]), close(pipefd[1]), perror("fork"), -1);
+	if (pid == 0)
+	{
+		close(pipefd[0]);
+		read_heredoc_lines(mini, delimiter, pipefd[1], cmd_node, builtin);
+		close(pipefd[1]);
+		exit(0);
+	}
 	close(pipefd[1]);
+	waitpid(pid, &status, 0);
+	if (WIFSIGNALED(status))
+		return (close(pipefd[0]), -1);
 	return (pipefd[0]);
 }

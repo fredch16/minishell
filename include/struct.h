33b163a2994dc0f7bc86701b588285b57630c95f
4n4k1n <anakin@pregitzer.de>
2025-06-15 18:00:21 +0200

    FEAT(structs.h) - added structs for the heredocs

--- include/struct.h
+++ include/struct.h
@@ -6,7 +6,7 @@
 /*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 23:28:02 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/08 07:40:52 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/15 17:58:56 by apregitz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,26 @@ typedef enum e_token_type
 	TK_BUILTIN,
 	TK_S_QUOTES
 }						t_token_type;
+
+typedef struct s_hd_line
+{
+	char				*line;
+	struct s_hd_line	*next;
+}						t_hd_line;
+
+typedef struct s_hd_node
+{
+	t_hd_node			*next;
+	t_hd_line			*lines;
+	char				*lim;
+}						t_hd_node;
+
+typedef struct s_hd_list
+{
+	t_hd_node			*head;
+	t_hd_node			*tail;
+	size_t				size;
+}						t_hd_list;
 
 typedef struct s_exec_data
 {

d9a8ddd306dfa513049c7e1769c2fe82624414d7
Fred Charbonnier <frederickcharbonnier@icloud.com>
2025-06-21 20:15:11 +0200

    FIX(heredoc sigquit) - added a signal reciever for sig quit which does nothing, but will catch it and stop it from printing ^"

--- src/signals.c
+++ src/signals.c
@@ -6,7 +6,7 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 16:35:11 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/20 16:58:16 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/06/21 20:13:50y fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,42 +31,67 @@ void	heredoc_sigint_handler(int signo)
 	exit(130);
 }
 
+/**
+ * Empty SIGQUIT handler to prevent ^\ from being printed
+ * This handler does nothing but exists to catch the signal
+ */
+void	empty_sigquit_handler(int signo)
+{
+	(void)signo;
+}
+
+void	setup_heredoc_signals(void)
+{
+	struct sigaction	sa_int, sa_quit;
+	struct termios		term;
+
+	// Get the current terminal attributes
+	tcgetattr(STDIN_FILENO, &term);
+	
+	// Disable ECHOCTL to prevent control characters from being displayed
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+
+	// Set up SIGINT handler
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_handler = heredoc_sigint_handler;
+	sa_int.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa_int, NULL);
+
+	// Set up SIGQUIT handler with empty function
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_handler = empty_sigquit_handler;
+	sa_quit.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
+
+/**
+ * Reset signal handlers and terminal settings for main shell
+ */
+void	reset_parent_signals(void)
+{
+	struct sigaction	sa_int, sa_quit;
+	struct termios		term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_lflag |= ECHOCTL;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+	sigemptyset(&sa_int.sa_mask);
+	sa_int.sa_handler = handle_sigint;
+	sa_int.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa_int, NULL);
+	sigemptyset(&sa_quit.sa_mask);
+	sa_quit.sa_handler = SIG_IGN;
+	sa_quit.sa_flags = SA_RESTART;
+	sigaction(SIGQUIT, &sa_quit, NULL);
+}
+
 void	setup_parent_signals_for_execution(void)
 {
 	struct sigaction	sa_int, sa_quit;
 
 	sigemptyset(&sa_int.sa_mask);
 	sa_int.sa_handler = SIG_IGN;
-	sa_int.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa_int, NULL);
-
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_handler = SIG_IGN;
-	sa_quit.sa_flags = SA_RESTART;
-	sigaction(SIGQUIT, &sa_quit, NULL);
-}
-
-void	reset_parent_signals(void)
-{
-	struct sigaction	sa_int, sa_quit;
-
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_handler = handle_sigint;
-	sa_int.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa_int, NULL);
-
-	sigemptyset(&sa_quit.sa_mask);
-	sa_quit.sa_handler = SIG_IGN;
-	sa_quit.sa_flags = SA_RESTART;
-	sigaction(SIGQUIT, &sa_quit, NULL);
-}
-
-void	setup_heredoc_signals(void)
-{
-	struct sigaction	sa_int, sa_quit;
-
-	sigemptyset(&sa_int.sa_mask);
-	sa_int.sa_handler = heredoc_sigint_handler;
 	sa_int.sa_flags = SA_RESTART;
 	sigaction(SIGINT, &sa_int, NULL);
 

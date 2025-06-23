52e1aa8aff2b2df50862f37acd885c9f7365acc4
Fred Charbonnier <frederickcharbonnier@icloud.com>
2025-06-23 18:22:33 +0200

    NORM(parser) - fixed parser completely norm conform TNT IM DYNAMITE

--- src/parser/_parser.c
+++ src/parser/_parser.c
@@ -6,7 +6,7 @@
 /*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 21:35:05 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/20 16:21:21 by fredchar         ###   ########.fr       */
+/*   Updated: 2025/06/23 18:21:25 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,29 @@ static void	finalize_command(t_cmd_node *cmd)
 		cmd->cmd_type = STDCMD;
 }
 
+void	build_pipe(t_cmd_list **cmd_list, t_cmd_node **current_cmd,
+	t_file_list **file_list)
+{
+	finalize_command(*current_cmd);
+	(*current_cmd)->files = *file_list;
+	cmd_add_back(*cmd_list, *current_cmd);
+	*current_cmd = new_cmd();
+	*file_list = init_file_list();
+}
+
+int	build_redirect(t_file_list **file_list, t_token_node **current_token,
+	t_token_list **token_list)
+{
+	if (!(*current_token)->next || (*current_token)->next->type != TK_CMD)
+	{
+		(*token_list)->error_code = 998;
+		return (1);
+	}
+	add_redirection(*file_list, *current_token, (*current_token)->next);
+	*current_token = (*current_token)->next;
+	return (0);
+}
+
 int	build_cmd_list(t_token_list *token_list, t_cmd_list *cmd_list)
 {
 	t_cmd_node		*current_cmd;
@@ -44,22 +67,11 @@ int	build_cmd_list(t_token_list *token_list, t_cmd_list *cmd_list)
 	while (current_token)
 	{
 		if (current_token->type == TK_PIPE)
-		{
-			finalize_command(current_cmd);
-			current_cmd->files = file_list;
-			cmd_add_back(cmd_list, current_cmd);
-			current_cmd = new_cmd();
-			file_list = init_file_list();
-		}
+			build_pipe(&cmd_list, &current_cmd, &file_list);
 		else if (is_redirect(current_token))
 		{
-			if (!current_token->next || current_token->next->type != TK_CMD)
-			{
-				token_list->error_code = 999;
-				return (token_list->error_code);
-			}
-			add_redirection(file_list, current_token, current_token->next);
-			current_token = current_token->next;
+			if (build_redirect(&file_list, &current_token, &token_list))
+				return (998);
 		}
 		else
 			add_arg_to_cmd(current_cmd, current_token);

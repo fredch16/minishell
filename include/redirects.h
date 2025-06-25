47020f0580034d02e3e959cc29d89e7f193f37e2
Fred Charbonnier <frederickcharbonnier@icloud.com>
2025-06-25 12:41:42 +0200

    NORM(include/redirects.h) - fixed norm

--- include/redirects.h
+++ include/redirects.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   redirects.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apregitz <apregitz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fredchar <fredchar@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/07 17:28:12 by fredchar          #+#    #+#             */
-/*   Updated: 2025/06/25 11:34:11 by apregitz         ###   ########.fr       */
+/*   Updated: 2025/06/25 12:40:59 by fredchar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 
 # include "minishell.h"
 
-char	*expand_heredoc(t_mini *mini, char *line);
+char		*expand_heredoc(t_mini *mini, char *line);
 
 t_hd_node	*get_heredoc_for_file(t_cmd_node *cmd_node,
-	t_file_node *file_node);
-int	process_input_redirection(t_file_node *file_node, t_mini *mini,
-        t_cmd_node *cmd_node, int builtins);
-int	process_output_redirection(t_file_node *file_node, t_mini *mini);
-int	dup_fd(int last_input_fd);
+				t_file_node *file_node);
+int			process_input_redirection(t_file_node *file_node, t_mini *mini,
+				t_cmd_node *cmd_node, int builtins);
+int			process_output_redirection(t_file_node *file_node, t_mini *mini);
+int			dup_fd(int last_input_fd);
 
 #endif
